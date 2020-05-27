359c13dd5c99a894afca285a7d2c4d4a3ef17d42
Jan Jakub Nanista <jan.jakub.nanista@gmail.com>
2020-05-27 17:20:23 +0200

    Test createLTOB as well

--- src/methods/__tests__/LTOB.spec.ts
+++ src/methods/__tests__/LTOB.spec.ts
@@ -1,46 +1,86 @@
 import 'jest';
-import { DataPoint } from '../../types';
-import { LTOB } from '../LTOB';
+import { DownsamplingFunction } from '../../types';
+import { LTOB, createLTOB } from '../LTOB';
 import { makeTupleDateTestData, makeTupleNumberTestData, makeXYDateTestData, makeXYNumberTestData } from './utils';
 import data from '../../../data/power.json';
 
 describe('LTOB', () => {
-  describe('with XYDataPoint with Date', () => testMethod(makeXYDateTestData(data)));
-  describe('with TupleDataPoint with Date', () => testMethod(makeTupleDateTestData(data)));
-  describe('with XYDataPoint with number', () => testMethod(makeXYNumberTestData(data)));
-  describe('with TupleDataPoint with number', () => testMethod(makeTupleNumberTestData(data)));
+  describe('with XYDataPoint with Date', () => testStuff(makeXYDateTestData(data), LTOB));
+  describe('with TupleDataPoint with Date', () => testStuff(makeTupleDateTestData(data), LTOB));
+  describe('with XYDataPoint with number', () => testStuff(makeXYNumberTestData(data), LTOB));
+  describe('with TupleDataPoint with number', () => testStuff(makeTupleNumberTestData(data), LTOB));
 
-  function testMethod(data: DataPoint[]) {
+  describe('createLTOB', () => {
+    describe('with string / number x / y', () => {
+      describe('with XYDataPoint with number', () =>
+        testStuff(
+          makeXYNumberTestData(data),
+          createLTOB({
+            x: 'x',
+            y: 'y',
+          }),
+        ));
+      describe('with TupleDataPoint with number', () =>
+        testStuff(
+          makeTupleNumberTestData(data),
+          createLTOB({
+            x: 0,
+            y: 1,
+          }),
+        ));
+    });
+
+    describe('with function x / y', () => {
+      describe('with XYDataPoint with Date', () =>
+        testStuff(
+          makeXYDateTestData(data),
+          createLTOB({
+            x: (value) => value.x.getTime(),
+            y: 'y',
+          }),
+        ));
+      describe('with TupleDataPoint with Date', () =>
+        testStuff(
+          makeTupleDateTestData(data),
+          createLTOB({
+            x: (value) => value[0].getTime(),
+            y: 1,
+          }),
+        ));
+    });
+  });
+
+  function testStuff<T>(data: T[], method: DownsamplingFunction<T, [number]>): void {
     it('should throw an error if desiredLength is negative', () => {
-      expect(() => LTOB(data, -1)).toThrow();
+      expect(() => method(data, -1)).toThrow();
     });
 
     it('should return the whole data set if there are two data points', () => {
-      expect(LTOB(data.slice(0, 2), 1)).toHaveLength(2);
+      expect(method(data.slice(0, 2), 1)).toHaveLength(2);
     });
 
     it('should return the whole data set if desiredLength is larger than the data set length', () => {
-      expect(LTOB(data, data.length + 40)).toHaveLength(data.length);
+      expect(method(data, data.length + 40)).toHaveLength(data.length);
     });
 
     it('should return desired number of data points', () => {
-      expect(LTOB(data, 3)).toHaveLength(3);
-      expect(LTOB(data, 5)).toHaveLength(5);
-      expect(LTOB(data, 7)).toHaveLength(7);
-      expect(LTOB(data, 8)).toHaveLength(8);
+      expect(method(data, 3)).toHaveLength(3);
+      expect(method(data, 5)).toHaveLength(5);
+      expect(method(data, 7)).toHaveLength(7);
+      expect(method(data, 8)).toHaveLength(8);
     });
 
     it('should preserve the first and last data points', () => {
-      expect(LTOB(data, 5)[0]).toEqual(data[0]);
-      expect(LTOB(data, 5)[4]).toEqual(data[data.length - 1]);
+      expect(method(data, 5)[0]).toEqual(data[0]);
+      expect(method(data, 5)[4]).toEqual(data[data.length - 1]);
     });
 
     it('should downsample correctly', () => {
-      expect(LTOB(data, 1)).toMatchSnapshot();
-      expect(LTOB(data, 2)).toMatchSnapshot();
-      expect(LTOB(data, 5)).toMatchSnapshot();
-      expect(LTOB(data, 1000)).toMatchSnapshot();
-      expect(LTOB(data, Math.round(data.length / 2))).toMatchSnapshot();
+      expect(method(data, 1)).toMatchSnapshot();
+      expect(method(data, 2)).toMatchSnapshot();
+      expect(method(data, 5)).toMatchSnapshot();
+      expect(method(data, 1000)).toMatchSnapshot();
+      expect(method(data, Math.round(data.length / 2))).toMatchSnapshot();
     });
   }
 });
